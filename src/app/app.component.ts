4edbca07c64e037f41aa1ea036a6757826d28a80
PeLuFe <fe.cornejo@duocuc.cl>
2022-09-14 15:50:23 -0300

    Creacion Formulario Seccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Seccion } from './modelos/seccion'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'asistenciaAPP';
+  public listaSeccion: Array<Seccion> = [
+    {idSeccion: 0,
+    nombre: ''},
+  ]
+  public agregarSeccion(nueva: Seccion): void {
+    const idNueva = this.listaSeccion.length + 1;
+    nueva.idSeccion = idNueva;
+    this.listaSeccion.push(nueva)
+  }
 }
--- src/app/componentes/formulario-seccion/formulario-seccion.component.ts
+++ src/app/componentes/formulario-seccion/formulario-seccion.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Seccion } from './../../modelos/seccion'
 
 @Component({
   selector: 'app-formulario-seccion',
   templateUrl: './formulario-seccion.component.html',
   styleUrls: ['./formulario-seccion.component.scss']
 })
-export class FormularioSeccionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormularioSeccionComponent {
+  @Output() public agregarSeccion = new EventEmitter<Seccion>();
+  public seccion: Seccion = {
+    nombre: ''
+  };
+  public cambiarNombre(evento: Event): void {
+    const elementoNombre = evento.target as HTMLInputElement;
+    this.seccion.nombre = elementoNombre.value;
   }
-
+  public guardarSeccion(): void {
+    console.log(this.seccion);
+    const copiaSeccion: Seccion = { ...this.seccion };
+    this.agregarSeccion.emit(copiaSeccion);
+    this.seccion.nombre = '';
+  }
 }
